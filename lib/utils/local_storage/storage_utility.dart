78799587ca0044949215f309b5ebd9589eece821
immicks <immicks@gmail.com>
2024-02-19 14:41:30 +0100

    finished structure

---
+++ lib/utils/local_storage/storage_utility.dart
@@ -0,0 +1,29 @@
+import 'package:get_storage/get_storage.dart';
+
+class CLocalStorage {
+  static final CLocalStorage _instance = CLocalStorage._internal();
+
+  factory CLocalStorage() {
+    return _instance;
+  }
+
+  CLocalStorage._internal();
+
+  final _storage = GetStorage();
+
+  Future<void> saveData<T>(String key, T value) async {
+    await _storage.write(key, value);
+  }
+
+  T? readData<T>(String key) {
+    return _storage.read<T>(key);
+  }
+
+  Future<void> removeData(String key) async {
+    await _storage.remove(key);
+  }
+
+  Future<void> clearAll() async {
+    await _storage.erase();
+  }
+}
